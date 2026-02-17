8b256b0abdde950a9fb724ca03d146859b645a40
André Guerra <2221460@my.ipleiria.pt>
2026-02-17 15:28:56 +0000

    Created Footer

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
@@ -107,13 +109,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    url: "https://github.com/AndreGuerra20",
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
     img: "/link.svg",
+    url: "https://www.linkedin.com/in/andr%C3%A9-guerra-700195371/",
   },
 ];
