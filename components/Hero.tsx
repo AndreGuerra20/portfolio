41ed23bcb3c8bc98b06fe0844491ac96ab319e8d
André Guerra <2221460@my.ipleiria.pt>
2026-02-17 14:50:34 +0000

    Removed unnecessary files and Added Logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,10 +3,11 @@ import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
 import MagicButton from './ui/MagicButton';
 import { FaLocationArrow } from 'react-icons/fa';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-4">
         <div>
             <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
             <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
@@ -22,6 +23,7 @@ const Hero = () => {
 
         <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                <Image src="/logo.png" alt="logo" width={300} height={150}/>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
                     Blockchain · Cybersecurity · IoT · Applied AI Research
                 </h2>
